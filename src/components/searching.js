a6b005b7ed6d91a8bb4a5a2f6f571593d6cd7609
Konstantin Zinchenko <govn000vp@yandex.ru>
2026-02-16 19:56:10 +0400

    Шестая проектная работа

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,12 @@
-import {rules, createComparison} from "../lib/compare.js";
+import {rules, createComparison, rules} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+    const search = createComparison(rules.skipEmptyTargetValues, 
+       [ rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]);
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => search(row, state));
     }
 }
