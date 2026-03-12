a6b286cc704ffbd433dfe1841819c86ea433cf28
Konstantin Zinchenko <govn000vp@yandex.ru>
2026-03-12 15:22:09 +0400

    Седьмая проектная работа

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,22 +1,9 @@
-import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
-    // Получаем правило поиска
-    const searchRule = rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false);
-    
-    // Создаём компаратор
-    const search = createComparison(
-        [], 
-        [rules.skipEmptyTargetValues, searchRule]
-    );
-    
-    return (data, state, action) => {
-        // Если поиск пустой - возвращаем все данные
-        if (!state[searchField] || state[searchField].trim() === '') {
-            return data;
-        }
         
-        // Иначе - фильтруем
-        return data.filter(row => search(row, state));
-    }
+ return (query, state, action) => { // result заменили на query
+    return state[searchField] ? Object.assign({}, query, { // проверяем, что в поле поиска было что-то введено
+        search: state[searchField] // устанавливаем в query параметр
+    }) : query; // если поле с поиском пустое, просто возвращаем query без изменений
+}
 }
